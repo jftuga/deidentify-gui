aa122f26c4c3d724cf41c87adbdf5a903e745a98
John Taylor <johntaylor@gmail.com>
2021-03-18 15:48:08 -0400

    WIP. It works in my enviornment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace deidentify_gui
 {
@@ -20,9 +25,13 @@ namespace deidentify_gui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string jsonFile;
+        private string outputFile;
         public MainWindow()
         {
             InitializeComponent();
+            jsonFile = "";
+            outputFile = "";
         }
 
         private void Click_Close(object sender, RoutedEventArgs e)
@@ -34,7 +43,7 @@ namespace deidentify_gui
         {
             get
             {
-                return "In tennis, Naomi Osaka wins the women's singles and Novak Djokovic wins the men's singles at the Australian Open (both winners pictured). Naomi Osaka defeated Jennifer Brady in the final, 6–4, 6–3 to win the Women's Singles tennis title at the 2021 Australian Open. This was Osaka's second Australian Open title and her fourth Grand Slam tournament singles title. At this event Osaka extended her winning streak, dating back to the 2020 Cincinnati Open, to 21 matches. Osaka dropped just one set the entire tournament, to Garbiñe Muguruza in the fourth round; Osaka also saved two match points in this fourth round match against Muguruza. Osaka became the first female player to win four Grand Slam singles titles since Maria Sharapova in 2012, and the youngest since Justine Henin won the 2005 French Open. Osaka became the third player in the Open Era, after Monica Seles and Roger Federer, to win their first four Grand Slam finals. Sofia Kenin was the defending champion, but she lost to Kaia Kanepi in the second round. Her loss to Kanepi was the earliest by a women's defending champion at the Australian Open since two-time defending champion Jennifer Capriati lost in the opening round in 2003. Mayar Sherif became the first Egyptian woman to win a match in a Grand Slam tournament main draw. Serena Williams equalled Chris Evert's all-time record of reaching 54 Grand Slam quarterfinals. Hsieh Su-wei became the first female Taiwanese player to make a Grand Slam singles quarterfinal. At 35 years old, she is also the oldest player to debut in a Grand Slam quarterfinal.";
+                return "";
             }
         }
 
@@ -42,8 +51,121 @@ namespace deidentify_gui
         {
             get
             {
-                return "In tennis, Naomi Osaka wins the women's singles and Novak Djokovic wins the men's singles at the Australian Open (both winners pictured). Naomi Osaka defeated Jennifer Brady in the final, 6–4, 6–3 to win the Women's Singles tennis title at the 2021 Australian Open. This was Osaka's second Australian Open title and her fourth Grand Slam tournament singles title. At this event Osaka extended her winning streak, dating back to the 2020 Cincinnati Open, to 21 matches. Osaka dropped just one set the entire tournament, to Garbiñe Muguruza in the fourth round; Osaka also saved two match points in this fourth round match against Muguruza. Osaka became the first female player to win four Grand Slam singles titles since Maria Sharapova in 2012, and the youngest since Justine Henin won the 2005 French Open. Osaka became the third player in the Open Era, after Monica Seles and Roger Federer, to win their first four Grand Slam finals. Sofia Kenin was the defending champion, but she lost to Kaia Kanepi in the second round. Her loss to Kanepi was the earliest by a women's defending champion at the Australian Open since two-time defending champion Jennifer Capriati lost in the opening round in 2003. Mayar Sherif became the first Egyptian woman to win a match in a Grand Slam tournament main draw. Serena Williams equalled Chris Evert's all-time record of reaching 54 Grand Slam quarterfinals. Hsieh Su-wei became the first female Taiwanese player to make a Grand Slam singles quarterfinal. At 35 years old, she is also the oldest player to debut in a Grand Slam quarterfinal.";
+                return "";
             }
+        }
+
+        string StringFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(
+                // TextPointer to the start of content in the RichTextBox.
+                rtb.Document.ContentStart,
+                // TextPointer to the end of content in the RichTextBox.
+                rtb.Document.ContentEnd
+            );
+
+            // The Text property on a TextRange object returns a string
+            // representing the plain text content of the TextRange.
+            return textRange.Text;
+        }
+
+        private void Click_Deidentify(object sender, RoutedEventArgs e)
+        {
+            TextBox_Status.Text = "Starting deidentification...";
+            jsonFile = @"r:\temp\test.txt";
+            File.WriteAllText(jsonFile, StringFromRichTextBox(RichTextBox_Original));
+            File_Deidentify();
+        }
+
+        private void File_Deidentify()
+        {
+            string filename = @"C:\github.com\jftuga\deidentify\activate.bat";
+            outputFile = @"r:\temp\output.txt";
+            string args = string.Format("-r {0} -o {1} {2}", TextBox_Replacement.Text, outputFile, jsonFile);
+            Console.WriteLine(args);
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = filename,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            RichTextBox_Deidentified.Document.Blocks.Clear();
+            proc.Start();
+            while (!proc.StandardError.EndOfStream)
+            {
+                string line = proc.StandardError.ReadLine();
+                TextBox_Status.Text = line;
+            }
+            string readText = File.ReadAllText(outputFile);
+            RichTextBox_Deidentified.Document.Blocks.Add(new Paragraph(new Run(readText)));
+        }
+
+
+        private void Click_OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string readText = "";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                RichTextBox_Original.Document.Blocks.Clear();
+                readText = File.ReadAllText(openFileDialog.FileName);
+                RichTextBox_Original.Document.Blocks.Add(new Paragraph(new Run(readText)));
+            }
+        }
+
+        private void Click_About(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Deidentify GUI" + "\n" + "version 1.0.0" + "\n\n" + "https://github.com/jftuga/deidentify-gui" + "\n" + "https://github.com/jftuga/deidentify", "Deidentify");
+        }
+
+        // https://www.codeproject.com/Tips/1273917/Opening-a-File-Specifically-in-the-Default-Browser
+        private static string GetPathToDefaultBrowser()
+        {
+            const string currentUserSubKey =
+            @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice";
+            using (RegistryKey userChoiceKey = Registry.CurrentUser.OpenSubKey(currentUserSubKey, false))
+            {
+                string progId = (userChoiceKey.GetValue("ProgId").ToString());
+                using (RegistryKey kp =
+                       Registry.ClassesRoot.OpenSubKey(progId + @"\shell\open\command", false))
+                {
+                    // Get default value and convert to EXE path.
+                    // It's stored as:
+                    //    "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -- "%1"
+                    // So we want the first quoted string only
+                    string rawValue = (string)kp.GetValue("");
+                    Regex reg = new Regex("(?<=\").*?(?=\")");
+                    Match m = reg.Match(rawValue);
+                    return m.Success ? m.Value : "";
+                }
+            }
+        }
+
+        private void Click_Debug(object sender, RoutedEventArgs e)
+        {
+            string browserPath = GetPathToDefaultBrowser();
+            string converted = @"R:\Temp\test--tokens.json";
+            Process.Start(browserPath, converted);
+        }
+
+        private void Click_Clear(object sender, RoutedEventArgs e)
+        {
+            RichTextBox_Original.Document.Blocks.Clear();
+            RichTextBox_Deidentified.Document.Blocks.Clear();
+            TextBox_Status.Text = "";
+        }
+
+        private void Click_Copy(object sender, RoutedEventArgs e)
+        {
+            RichTextBox_Deidentified.SelectAll();
+            RichTextBox_Deidentified.Copy();
         }
     }
 }
