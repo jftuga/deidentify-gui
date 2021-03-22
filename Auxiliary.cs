bda1349b9cec5ed7644786623dec0906a93249bf
John Taylor <johntaylor@gmail.com>
2021-03-22 11:18:24 -0400

    move some functions to Auxiliary.cs; misc clean ups

---
+++ Auxiliary.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace deidentify_gui
+{
+    class Auxiliary
+    {
+        public static string StringFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(
+                rtb.Document.ContentStart,
+                rtb.Document.ContentEnd
+            );
+
+            return textRange.Text;
+        }
+
+        // https://www.codeproject.com/Tips/1273917/Opening-a-File-Specifically-in-the-Default-Browser
+        public static string GetPathToDefaultBrowser()
+        {
+            const string currentUserSubKey =
+            @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice";
+            using (RegistryKey userChoiceKey = Registry.CurrentUser.OpenSubKey(currentUserSubKey, false))
+            {
+                string progId = (userChoiceKey.GetValue("ProgId").ToString());
+                using (RegistryKey kp =
+                       Registry.ClassesRoot.OpenSubKey(progId + @"\shell\open\command", false))
+                {
+                    string rawValue = (string)kp.GetValue("");
+                    Regex reg = new Regex("(?<=\").*?(?=\")");
+                    Match m = reg.Match(rawValue);
+                    return m.Success ? m.Value : "";
+                }
+            }
+        }
+
+        public static void FileDeidentify(string replacementText, string outputFile, string inputFile, Label label)
+        {
+            // FIXME - When entering text (and not using Open), this button must be pressed twice
+            string filename = @"python";
+            string args = string.Format(@"deidentify.py -H -r {0} -o {1} {2}", replacementText, outputFile, inputFile);
+
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = filename,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            label.Content = "Status:";
+            proc.Start();
+            string line = "";
+            while (!proc.StandardError.EndOfStream)
+            {
+                line = proc.StandardError.ReadLine();
+                label.Content = "Status: " + line;
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Path = System.IO.Path;
 
+
 namespace deidentify_gui
 {
     /// <summary>
@@ -33,11 +35,11 @@ namespace deidentify_gui
         public MainWindow()
         {
             InitializeComponent();
-            resetDefaults();
+            ResetDefaults();
 
         }
 
-        private void resetDefaults()
+        private void ResetDefaults()
         {
             string temp = (Environment.GetEnvironmentVariable("TEMP").Length > 0) ? Environment.GetEnvironmentVariable("TEMP") : (Environment.GetEnvironmentVariable("TMP").Length > 0) ? Environment.GetEnvironmentVariable("TMP") : ".";
             temp += Path.DirectorySeparatorChar;
@@ -48,21 +50,6 @@ namespace deidentify_gui
             this.usingDefaultFilenames = true;
         }
 
-        private void Click_Close(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        string StringFromRichTextBox(RichTextBox rtb)
-        {
-            TextRange textRange = new TextRange(
-                rtb.Document.ContentStart,
-                rtb.Document.ContentEnd
-            );
-
-            return textRange.Text;
-        }
-
         private void SetAllFilename()
         {
             string fileNameWithoutExtension = this.inputFile;
@@ -70,59 +57,36 @@ namespace deidentify_gui
             if (this.usingDefaultFilenames == false)
             {
                 int i = this.inputFile.LastIndexOf(".");
-                
-                if ( i > 0 )
+
+                if (i > 0)
                 {
                     fileNameWithoutExtension = this.inputFile.Substring(0, i);
                 }
                 this.jsonFile = fileNameWithoutExtension + "--tokens.json";
                 this.outputFile = fileNameWithoutExtension + "--deidentified" + ".htm";
-                Console.WriteLine("{0} {1} {2}", this.inputFile, this.jsonFile, this.outputFile);
-                Console.WriteLine();
             }
+        }
+
+        private void Click_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
 
         private void Click_Deidentify(object sender, RoutedEventArgs e)
         {
             Label_Status.Content = "Status: Starting deidentification...";
             if ( usingDefaultFilenames ) {
-                File.WriteAllText(this.inputFile, StringFromRichTextBox(RichTextBox_Original));
+                File.WriteAllText(this.inputFile, Auxiliary.StringFromRichTextBox(RichTextBox_Original));
             }
-            File_Deidentify();
-        }
-
-        private void File_Deidentify()
-        {
-            // FIXME - When entering text (and not using Open), this button must be pressed twice
-            string filename = @"C:\github.com\jftuga\deidentify\activate.bat"; //FIXME - don't hard code path
-            string args = string.Format("-H -r {0} -o {1} {2}", TextBox_Replacement.Text, this.outputFile, this.inputFile);
-            Console.WriteLine(args);
-            var proc = new Process
+            try
             {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = filename,
-                    Arguments = args,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    CreateNoWindow = true
-                }
-            };
-
-            Label_Status.Content = "Status:";
-            proc.Start();
-            string line = "";
-            while (!proc.StandardError.EndOfStream)
+                Auxiliary.FileDeidentify(TextBox_Replacement.Text, this.outputFile, this.inputFile, Label_Status);
+                myBrowser.Navigate(this.outputFile);
+            } catch( Exception ex)
             {
-                line = proc.StandardError.ReadLine();
-                Label_Status.Content = "Status: " + line;
+                MessageBox.Show(ex.ToString(), "Exception #18293");
             }
-
-            proc.StandardError.ReadLine();
-            myBrowser.Navigate(this.outputFile);
         }
-
 
         private void Click_OpenFile(object sender, RoutedEventArgs e)
         {
@@ -141,34 +105,15 @@ namespace deidentify_gui
 
         private void Click_About(object sender, RoutedEventArgs e)
         {
-            //FIXME - don't hard code version number
-            MessageBox.Show("Deidentify GUI" + "\n" + "version 1.0.0" + "\n\n" + "https://github.com/jftuga/deidentify-gui" + "\n" + "https://github.com/jftuga/deidentify", "Deidentify");
-        }
-
-        // https://www.codeproject.com/Tips/1273917/Opening-a-File-Specifically-in-the-Default-Browser
-        private static string GetPathToDefaultBrowser()
-        {
-            const string currentUserSubKey =
-            @"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice";
-            using (RegistryKey userChoiceKey = Registry.CurrentUser.OpenSubKey(currentUserSubKey, false))
-            {
-                string progId = (userChoiceKey.GetValue("ProgId").ToString());
-                using (RegistryKey kp =
-                       Registry.ClassesRoot.OpenSubKey(progId + @"\shell\open\command", false))
-                {
-                    string rawValue = (string)kp.GetValue("");
-                    Regex reg = new Regex("(?<=\").*?(?=\")");
-                    Match m = reg.Match(rawValue);
-                    return m.Success ? m.Value : "";
-                }
-            }
+            ApplicationEnvironment app = PlatformServices.Default.Application;
+            string version = app.ApplicationVersion;
+            MessageBox.Show("Deidentify GUI" + "\n" + version + "\n\n" + "https://github.com/jftuga/deidentify-gui" + "\n" + "https://github.com/jftuga/deidentify", "Deidentify");
         }
 
         private void Click_Debug(object sender, RoutedEventArgs e)
         {
             string j = (usingDefaultFilenames) ? this.jsonFile + "default--input--tokens.json" : this.jsonFile;
-
-            string browserPath = GetPathToDefaultBrowser();
+            string browserPath = Auxiliary.GetPathToDefaultBrowser();
             Process.Start(browserPath, j);
         }
 
@@ -177,29 +122,25 @@ namespace deidentify_gui
             RichTextBox_Original.Document.Blocks.Clear();
             myBrowser.Navigate(@"about:blank");
             Label_Status.Content = "Status:";
-            resetDefaults();
+            ResetDefaults();
         }
 
         private void Click_Copy(object sender, RoutedEventArgs e)
         {
-
             var htmlDoc = new HtmlDocument();
             htmlDoc.Load(this.outputFile);
             var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//body");
 
-            //FIXME - don't break after one iteration
-            foreach (var node in htmlNodes)
+            if(htmlNodes.Count() > 0)
             {
-                Console.WriteLine(node.InnerHtml);
-                Clipboard.SetDataObject(new DataObject(DataFormats.Text, node.InnerText), true);
-                break;
-            }
+                Clipboard.SetDataObject(new DataObject(DataFormats.Text, htmlNodes[0].InnerText), true);
 
+            }
         }
 
         private void Click_Into_Original(object sender, MouseButtonEventArgs e)
         {
-            string o = StringFromRichTextBox(RichTextBox_Original);
+            string o = Auxiliary.StringFromRichTextBox(RichTextBox_Original);
             if ( o.StartsWith("Original Text") )
             {
                 RichTextBox_Original.Document.Blocks.Clear();
